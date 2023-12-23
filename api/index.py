6e2f6ef78915178544ec1c7f4dbc2df1c10e067b
Atiksh Vaish <atikshvaish@gmail.com>
2023-12-22 19:59:00 -0500

    GPT_output

--- api/index.py
+++ api/index.py
@@ -1,14 +1,16 @@
 from flask import Flask
-import os
+# import os
 
-test = os.environ.get("MYAPIKEY")
+# test = os.environ.get("MYAPIKEY")
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return test
+    return "Hello, World!"
 
-@app.route('/about')
-def about():
-    return 'About'
+
+@app.route('GPT_output')
+def GPT(): 
+    return("Hello this is chatGPT here")
+
