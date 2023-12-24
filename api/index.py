a0c962b9d6080138e2cfbc0a9dd254a0747e3b18
Atiksh Vaish <atikshvaish@gmail.com>
2023-12-24 14:58:04 -0500

    update index.py

--- api/index.py
+++ api/index.py
@@ -26,7 +26,7 @@ def speech():
         print(passkey)
 
         name = request.form["name"]
-        messages.append({"role": "system", "content": "My name is " + name + "."})
+        messages.append({"role": "system", "content": "My name is " + name + ".Try to refer to me by my name often. "})
 
         if passkey == PASSWORD:
             response = make_response("hello")
