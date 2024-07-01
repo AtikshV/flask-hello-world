d9a814ef5997b287c1bd4bb4ea8b4a31728ddeb1
Atiksh Vaish <atikshvaish@gmail.com>
2024-07-01 18:58:56 -0400

    new GPT_output_msg API

--- api/index.py
+++ api/index.py
@@ -2,6 +2,8 @@ from flask import Flask, make_response, request
 from flask_cors import CORS
 import os
 import openai
+from urllib.parse import unquote
+import json
 
 
 openai.api_key = os.environ.get("MYAPIKEY")
@@ -21,7 +23,7 @@ def home():
 @app.route('/GPT_output', methods=["POST"])
 def speech():
         user_input = request.form["data"]
-        
+        print(user_input)
         passkey = request.form["pass"]
         print(passkey)
 
@@ -46,5 +48,33 @@ def speech():
         else:
             return("sorry")
         
+@app.route('/GPT_output_msg', methods=["POST"])
+def speech_msg():
+        messages_new = json.loads(unquote(request.form["data"]))
+        print(messages_new)
+        passkey = request.form["pass"]
+        print(passkey)
+
+        # name = request.form["name"]
+        # messages.append({"role": "system", "content": "My name is " + name + ".Try to refer to me by my name often. "})
+
+        if passkey == PASSWORD:
+            response = make_response("hello")
+            response.headers["Access-Control-Allow-Origin"] = "*"
+            # print(request.form["data"])
+            # messages.append({"role": "user", "content": user_input})
+            response = openai.ChatCompletion.create(
+                model = "gpt-3.5-turbo",
+                messages = messages_new
+            )
+            ChatGPT_reply = response["choices"][0]["message"]["content"]
+            # messages.append({"role": "assistant", "content": ChatGPT_reply})
+            
+            # return jsonify({"response": ChatGPT_reply})
+            print(messages_new)
+            print(ChatGPT_reply)
+            return(ChatGPT_reply)
+        else:
+            return("sorry")
 
     
